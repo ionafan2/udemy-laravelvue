32a43b6fca0f40f40ac266b6cbcf49304b4c82de
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-06-02 16:30:17 -0700

    22. Component state and reactivity

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
         <bookable-list-item
-            item-title="Cheap villa"
-            item-content="A very chip villa"
+            :item-title="bookable1.title"
+            :item-content="bookable1.content"
             :price="1500">
 
         </bookable-list-item>
-        <bookable-list-item item-title="Cheap villa" item-content="A very chip villa"></bookable-list-item>
+
+        <bookable-list-item
+            :item-title="bookable2.title"
+            :item-content="bookable2.content"
+            :price="1500">
+
+        </bookable-list-item>
         <bookable-list-item></bookable-list-item>
         <bookable-list-item></bookable-list-item>
         <bookable-list-item></bookable-list-item>
@@ -21,9 +27,28 @@ export default {
     components: {
         BookableListItem
     },
+    data() {
+        return {
+            bookable1 : {
+                title: 'Cheap villa',
+                content: 'Very Cheap villa'
+            },
+            bookable2 : {
+                title: 'Cheap villa 2',
+                content: 'Very Cheap villa'
+            },
+        }
+    },
     beforeCreate() {
     },
     created() {
+        console.log(this.bookable1);
+        console.log(this.bookable2);
+        setTimeout(() => {
+            this.bookable1.title = "Expensive villa";
+            this.bookable2.title = "Very Expensive villa";
+        }, 5000);
+
     },
     beforeMount() {
     },
