9271647f9021b0c5cf9bb83e7462c0bca157222f
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-06-06 19:54:58 -0700

    45. API Resources

--- app/Http/Resources/BookableShowResource.php
+++ app/Http/Resources/BookableShowResource.php
@@ -17,7 +17,8 @@ class BookableShowResource extends JsonResource
         return [
             'id' => $this->id,
             'title'=> $this->title,
-            'description' => $this->description
+            'description' => $this->description,
+            'price' => $this->price
         ];
     }
 }
