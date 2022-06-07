b4e644d7ce7c1e883e3fe944ec457cfd8e624d7c
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-06-06 19:36:28 -0700

    43. Laravel controllers

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Bookable;
+use App\Http\Controllers\Api\BookableController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,11 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('bookables', function (Request $request) {
-    return Bookable::all();
-});
-
-
-Route::get('bookables/{id}', function (Request $request, $id) {
-    return Bookable::findOrFail($id);
-});
+Route::resource('bookables', BookableController::class)->only(['index', 'show']);
