14fbdb8578c557536d30ed56168fc2add09036cf
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-06-11 21:08:58 -0700

    62. Review model factory & seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             BookableTableSeeder::class,
-            BookingsTableSeeder::class
+            BookingsTableSeeder::class,
+            ReviewsTableSeeder::class,
         ]);
     }
 }
