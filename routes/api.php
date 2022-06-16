78677435a95c7135a5eab120cf68351b82016a9b
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-06-15 20:07:50 -0700

    75. Checking if review already exists on backend

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\BookableAvailabilityController;
 use App\Http\Controllers\Api\BookableController;
 use App\Http\Controllers\Api\BookableReviewController;
+use App\Http\Controllers\Api\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,3 +28,5 @@ Route::get('bookables/{bookable}/availability', BookableAvailabilityController::
 
 Route::get('bookables/{bookable}/reviews', BookableReviewController::class)
     ->name('bookables.reviews.index');
+
+Route::apiResource('reviews', ReviewController::class)->only(['show']);
