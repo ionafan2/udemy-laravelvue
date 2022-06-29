67de02029b73bbac6aeb1cf15bafafb635f4468e
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-06-28 17:08:40 -0700

    120. Validating arrays of input

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bookable;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
@@ -16,6 +17,10 @@ class CheckoutController extends Controller
     public function __invoke(Request $request)
     {
         $data = $request->validate([
+            'bookings' => 'required|array|min:1',
+            'bookings.*.bookable_id' => 'required|exists:bookables,id',
+            'bookings.*.startDate' => 'required|date|after_or_equal:today',
+            'bookings.*.endDate' => 'required|date|after_or_equal:bookings.*.startDate',
             'customer.first_names' => 'required|min:2',
             'customer.last_name' => 'required|min:2',
             'customer.street' => 'required|min:3',
@@ -25,5 +30,15 @@ class CheckoutController extends Controller
             'customer.state' => 'required|min:2',
             'customer.zip' => 'required|min:2'
         ]);
+
+        $data = array_merge($data, $request->validate([
+            'bookings.*' => ['required', function ($attribute, $value, $fail) {
+                $bookable = Bookable::findOrFail($value['bookable_id']);
+
+                if (!$bookable->availableFor($value['startDate'], $value['endDate'])) {
+                    $fail("The object is not available in given dates!");
+                }
+            }]
+        ]));
     }
 }
