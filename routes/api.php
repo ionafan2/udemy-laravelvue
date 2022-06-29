3542eb03db0159065648728dce95915b530a6f9c
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-06-28 22:03:48 -0700

    133. Understanding authentication with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResource('bookables', BookableController::class)->only(['index', 'show']);
 Route::get('bookables/{bookable}/availability', BookableAvailabilityController::class)
     ->name('bookables.availability.show');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes();
+
+Route::middleware('auth')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 
 Route::get('/{any?}', function () {
     return view('welcome');
