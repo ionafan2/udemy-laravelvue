d34c423e26be22256a65305bbf6a687ee6d3539b
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-06-28 21:01:12 -0700

    128. Rendering purchase confirmation

--- resources/js/basket/Basket.vue
+++ resources/js/basket/Basket.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <div class="row">
+        <success v-if="success">Congratulations on your purchase!</success>
+        <div class="row" v-else>
             <div class="col-md-8" v-if="itemsInBasket">
                 <div class="row">
                     <div class="col-md-6 mb-3">
@@ -123,6 +124,7 @@ export default {
     data() {
         return {
             loading: false,
+            bookingAttempted: false,
             customer: {
                 first_names: null,
                 last_name: null,
@@ -139,12 +141,16 @@ export default {
         ...mapGetters(["itemsInBasket"]),
         ...mapState({
             basket: state => state.basket.items
-        })
+        }),
+        success() {
+            return !this.loading && 0 === this.itemsInBasket && this.bookingAttempted;
+        }
     },
     methods: {
         async book() {
             this.loading = true;
             this.errors = false;
+            this.bookingAttempted = false;
 
             try {
                 await axios.post(`/api/checkout`, {
@@ -162,6 +168,7 @@ export default {
             }
 
             this.loading = false;
+            this.bookingAttempted = true;
         }
     }
 };
