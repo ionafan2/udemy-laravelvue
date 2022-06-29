92e912e9e1cb8cc23a928970e1b62a8802bab919
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-06-28 17:12:57 -0700

    121. Storing booking and address

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Address;
 use App\Models\Bookable;
+use App\Models\Booking;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
@@ -40,5 +42,27 @@ class CheckoutController extends Controller
                 }
             }]
         ]));
+
+
+        $bookingsData = $data['bookings'];
+        $addressData = $data['customer'];
+
+        $bookings = collect($bookingsData)->map(function ($bookingData) use ($addressData) {
+
+            $booking = new Booking();
+            $booking->bookable_id = $bookingData['bookable_id'];
+            $booking->startDate = $bookingData['startDate'];
+            $booking->endDate = $bookingData['endDate'];
+
+            $booking->price = 200;
+
+            $booking->address()->associate(Address::create($addressData));
+
+            $booking->save();
+
+            return $booking;
+        });
+
+        return $bookings;
     }
 }
--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -23,6 +23,11 @@ class Booking extends Model
         return $this->hasOne(Review::class);
     }
 
+    public function address()
+    {
+        return $this->belongsTo(Address::class);
+    }
+
     public function scopeBetweenDates(Builder $builder, $startDate, $endDate)
     {
         return $builder->where('endDate', '>=', $startDate)
