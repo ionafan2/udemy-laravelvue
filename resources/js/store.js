ec094fea8f6603e85de2dfa71a046c3b815d20f3
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-06-29 15:40:39 -0700

    141. Fixing "is logged in" delay

--- resources/js/store.js
+++ resources/js/store.js
@@ -52,6 +52,8 @@ export default {
                 context.commit('setBasket', JSON.parse(basket));
             }
 
+            context.commit("setLoggedIn", isLoggedIn());
+
         },
         addToBasket({commit, state}, payload) {
             commit('addToBasket', payload);
